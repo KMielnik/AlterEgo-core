7733e4993ac7b86c80401300cd0d36799f57c342
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2020-06-13 23:39:49 +0200

    Moved files to tool folder,changed run to function

--- tool/animate.py
+++ tool/animate.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 
-from frames_dataset import PairedDataset
-from logger import Logger, Visualizer
+from tool.frames_dataset import PairedDataset
+from tool.logger import Logger, Visualizer
 import imageio
 from scipy.spatial import ConvexHull
 import numpy as np
